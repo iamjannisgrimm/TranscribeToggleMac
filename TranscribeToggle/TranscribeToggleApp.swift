1ec1b0733a3012449097dfcd9e9151b0c500fa18
Jannis <jannis@grimm.me>
2025-09-13 20:30:47 -0400

    Initial Commit

---
+++ TranscribeToggle/TranscribeToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscribeToggleApp.swift
+//  TranscribeToggle
+//
+//  Created by Jannis Grimm on 9/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TranscribeToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
